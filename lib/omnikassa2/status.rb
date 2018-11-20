ca03d9db4379445bf74d95f201940d9202569e6b
Aike de Jongste <aikedejongste@gmail.com>
2018-11-20 13:29:59 +0100

    verify signature

--- lib/omnikassa2/status.rb
+++ lib/omnikassa2/status.rb
@@ -1,6 +1,6 @@
 module Omnikassa2
   class Status
-    attr_accessor :notification_token
+    attr_accessor :notification_token, :data
 
     def initialize(notification_token)
       self.notification_token = notification_token
@@ -15,7 +15,45 @@ module Omnikassa2
       req = Net::HTTP::Get.new(Omnikassa2::Status.uri)
       req['Authorization'] = "Bearer #{notification_token}"
       @res = Net::HTTP.start(Omnikassa2::Status.uri.hostname, Omnikassa2::Status.uri.port, use_ssl: true) { |http| http.request(req) }
-      JSON.parse(@res.body)
+      self.data = JSON.parse(@res.body)
+      data if verify_signature
+    end
+
+    def verify_signature
+      OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha512'), Omnikassa2.signing_key, data_string) == data['signature']
+    end
+
+    def data_string
+      keys.map do |key|
+        path = key.split(' ')
+        path.inject(data) { |memo, value| memo.fetch(value, '').to_s }
+      end.join(",") + data['orderResults'].map do |order_data|
+        order_keys do |key|
+          path = key.split(' ')
+          path.inject(order_data) { |memo, value| memo.fetch(value, '').to_s }
+        end.join(",")
+      end
+    end
+
+    def keys
+      [
+        'moreOrderResultsAvailable'
+      ]
+    end
+
+    def order_keys
+      [
+        'merchantOrderId',
+        'omnikassaOrderId',
+        'poiId',
+        'orderStatus',
+        'orderStatusDateTime',
+        'errorCode',
+        'paidAmount currency',
+        'paidAmount amount',
+        'totalAmount currency',
+        'totalAmount amount '
+      ]
     end
 
     def results

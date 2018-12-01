de5ab6f022051746302cdc1bdb3e177c8dd12767
Luc Zwanenberg <luc.zwanenberg@kabisa.nl>
2018-12-01 10:42:35 +0100

    Revert "Create Omnikassa2API stub"
    This reverts commit daa397c4523837caa4fa79a54903940672098b3d.

--- lib/omnikassa2/requests/refresh_request.rb
+++ lib/omnikassa2/requests/refresh_request.rb
@@ -1,19 +1,19 @@
 require 'omnikassa2/responses/refresh_response'
 
-class Omnikassa2::RefreshRequest
-  def send
-    req = Net::HTTP::Get.new(uri)
+module Omnikassa2
+  class RefreshRequest
+    def send
+      req = Net::HTTP::Get.new(uri)
+      req['Authorization'] = "Bearer #{Omnikassa2.refresh_token}"
+      http_response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
+      Omnikassa2::RefreshResponse.new(http_response)
+    end
 
-    req['Authorization'] = "Bearer #{Omnikassa2.refresh_token}"
+    private
 
-    http_response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) { |http| http.request(req) }
-    Omnikassa2::RefreshResponse.new(http_response)
-  end
-
-  private
-
-  def uri
-    tmp_url = Omnikassa2.base_url + '/gatekeeper/refresh'
-    URI(tmp_url)
+    def uri
+      tmp_url = Omnikassa2.base_url + '/gatekeeper/refresh'
+      URI(tmp_url)
+    end
   end
 end
--- spec/helpers/create.rb
+++
@@ -1,7 +0,0 @@
-def create(factory, params = {})
-  # could use reflection here
-  case factory
-  when :refresh_response
-    RefreshResponseFactory.create params
-  end
-end

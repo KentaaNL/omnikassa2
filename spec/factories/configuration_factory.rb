5afc5ce3e40f59936e9babc59b194d72901395fa
Luc Zwanenberg <luc.zwanenberg@kabisa.nl>
2018-12-01 13:09:26 +0100

    Improve refresh request spec

---
+++ spec/factories/configuration_factory.rb
@@ -0,0 +1,9 @@
+class ConfigurationFactory
+  def self.create(params = {})
+    {
+      refresh_token: params.fetch(:refresh_token, 'reFresht0ken'),
+      base_url: params.fetch(:base_url, 'https://www.example.com/sandbox'),
+      signing_key: params.fetch(:signing_key, 'sIgningK3y')
+    }
+  end
+end
--- spec/requests/refresh_request_spec.rb
+++ spec/requests/refresh_request_spec.rb
@@ -1,27 +1,61 @@
 describe Omnikassa2::RefreshRequest do
-  before(:all) do
+  before(:each) do
     Omnikassa2.config(
-      refresh_token: 'reFresht0ken',
-      base_url: 'https://www.example.com/sandbox',
-      signing_key: 'sIgningK3y'
+      ConfigurationFactory.create(
+        refresh_token: 'reFresht0ken',
+        base_url: 'https://www.example.com/sandbox'
+      )
     )
 
-    stub_request(:get, "https://www.example.com/sandbox/gatekeeper/refresh")
-      .with(body: nil, headers: {
-        'Authorization' => 'Bearer reFresht0ken'
-      })
+    WebMock.stub_request(:any, //)
       .to_return(
         body: {
-          token: 'accEssT0ken',
+          token: 'myAccEssT0ken',
           validUntil:  "2016-11-24T16:54:51.216+0000",
           durationInMillis: 28800000
         }.to_json
       )
   end
 
-  it 'correctly sends request' do
-    request = Omnikassa2::RefreshRequest.new
-    response = request.send
-    expect(response.access_token.token).to eql('accEssT0ken')
+  context 'when sent' do
+    it 'only invokes one requests' do
+      Omnikassa2::RefreshRequest.new.send
+      assert_requested :any, //, times: 1
+    end
+
+    it 'uses correct HTTP method' do
+      Omnikassa2::RefreshRequest.new.send
+      assert_requested :get, //, times: 1
+    end
+
+    it 'uses correct URL' do
+      Omnikassa2::RefreshRequest.new.send
+      assert_requested :any, 'https://www.example.com/sandbox/gatekeeper/refresh', times: 1
+    end
+
+    it 'sets header: \'Authorization: Bearer <refresh-token>\'' do
+      Omnikassa2::RefreshRequest.new.send
+      assert_requested :any, //, headers: {'Authorization' => 'Bearer reFresht0ken'}, times: 1
+    end
+
+    describe 'returned response' do
+      let(:returned_response) { Omnikassa2::RefreshRequest.new.send }
+
+      describe 'access token' do
+        let(:access_token) { returned_response.access_token }
+
+        it 'contains token' do
+          expect(access_token.token).to eql('myAccEssT0ken')
+        end
+  
+        it 'contains valid_until' do
+          expect(access_token.valid_until).to eql(Time.parse('2016-11-24T16:54:51.216+0000'))
+        end
+
+        it 'contains duration_in_millis' do
+          expect(access_token.duration_in_millis).to eql(28800000)
+        end
+      end
+    end
   end
 end

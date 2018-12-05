8d613040277de49d01ab41da4d9693be8e54e04d
Luc Zwanenberg <luc.zwanenberg@kabisa.nl>
2018-12-05 11:48:34 +0100

    Manually require all files

--- lib/omnikassa2.rb
+++ lib/omnikassa2.rb
@@ -2,11 +2,28 @@ require 'openssl'
 require 'net/http'
 require 'base64'
 
-[
-  "lib/**/*.rb"
-].each do |path|
-  Dir[File.expand_path path].each{|f| require_relative(f)}
-end
+require 'omnikassa2/version'
+
+require 'omnikassa2/helpers/access_token_provider'
+require 'omnikassa2/helpers/csv_serializer'
+require 'omnikassa2/helpers/signature_provider'
+
+require 'omnikassa2/models/access_token'
+require 'omnikassa2/models/merchant_order'
+require 'omnikassa2/models/money'
+require 'omnikassa2/models/notification'
+require 'omnikassa2/models/order_result_set'
+require 'omnikassa2/models/order_result'
+
+require 'omnikassa2/requests/base_request'
+require 'omnikassa2/requests/order_announce_request'
+require 'omnikassa2/requests/refresh_request'
+require 'omnikassa2/requests/status_pull_request'
+
+require 'omnikassa2/responses/base_response'
+require 'omnikassa2/responses/order_announce_response'
+require 'omnikassa2/responses/refresh_response'
+require 'omnikassa2/responses/status_pull_response'
 
 module Omnikassa2
   @@configured = false
